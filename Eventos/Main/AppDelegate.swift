5366f5a83ecc350b16e5c90f26da0229827ade5a
André Almeida <andre.almeida@iteris.com.br>
2022-04-05 19:03:26 -0300

    lint code

--- Eventos/Main/AppDelegate.swift
+++ Eventos/Main/AppDelegate.swift
@@ -9,12 +9,9 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    
     var window: UIWindow?
-    
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
         let rootViewController = ViewController()
         let navigagtionController = UINavigationController(rootViewController: rootViewController)
         window = UIWindow(frame: UIScreen.main.bounds)
