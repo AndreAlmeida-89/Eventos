bfd84e76b3d39406c615442624b540f5bf0c772c
André Almeida <andre.almeida@iteris.com.br>
2022-04-06 08:05:18 -0300

    add RxSwift and RxCocoa dependencies

--- EventosUITests/EventosUITests.swift
+++ EventosUITests/EventosUITests.swift
@@ -7,24 +7,24 @@
 
 import XCTest
 
-class EventosUITests: XCTestCase {
-
-    override func setUpWithError() throws {
-        continueAfterFailure = false
-    }
-
-    override func tearDownWithError() throws {}
-
-    func testExample() throws {
-        let app = XCUIApplication()
-        app.launch()
-    }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
-}
+//class EventosUITests: XCTestCase {
+//
+//    override func setUpWithError() throws {
+//        continueAfterFailure = false
+//    }
+//
+//    override func tearDownWithError() throws {}
+//
+//    func testExample() throws {
+//        let app = XCUIApplication()
+//        app.launch()
+//    }
+//
+//    func testLaunchPerformance() throws {
+//        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
+//            measure(metrics: [XCTApplicationLaunchMetric()]) {
+//                XCUIApplication().launch()
+//            }
+//        }
+//    }
+//}
