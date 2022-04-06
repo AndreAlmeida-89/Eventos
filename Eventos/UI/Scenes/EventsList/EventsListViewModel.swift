104add935ab9f7b79503f662a50f8ace173345e5
André Almeida <andre.almeida@iteris.com.br>
2022-04-06 10:33:40 -0300

    add EventsListViewModel logic

--- Eventos/UI/Scenes/EventsList/EventsListViewModel.swift
+++ Eventos/UI/Scenes/EventsList/EventsListViewModel.swift
@@ -6,11 +6,35 @@
 //
 
 import Foundation
+import RxSwift
 
 protocol EventsListViewModelContract {
-    
+    func getEvents()
+    var events: PublishSubject<[Event]> { get set }
 }
 
-class EventsListViewModel {
-    
+class EventsListViewModel: EventsListViewModelContract {
+    private let getEventsService: GetEventsServicing
+    private var disposeBag = DisposeBag()
+
+    var events: PublishSubject<[Event]> = PublishSubject()
+    let error: PublishSubject<NetworkError> = PublishSubject()
+    let loading: PublishSubject<Bool> = PublishSubject()
+
+    init(eventsService: GetEventsServicing) {
+        self.getEventsService = eventsService
+    }
+
+    func getEvents() {
+        loading.onNext(true)
+        getEventsService.getEvents { (result: Response<[Event]>) in
+            self.loading.onNext(false)
+            switch result {
+            case .success(let events):
+                self.events.onNext(events)
+            case .failure(let error):
+                self.error.onNext(error)
+            }
+        }
+    }
 }
